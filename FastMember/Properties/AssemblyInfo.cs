6aaffa36f777309c0409c76a449d21c35266ccdd
axunonb <nb@axuno.net>
2016-12-20 23:47:55 +0100

    Added missing #define COREFX and CoreFx methods

--- FastMember/Properties/AssemblyInfo.cs
+++ FastMember/Properties/AssemblyInfo.cs
@@ -23,6 +23,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("fa8f50b2-45ec-46ee-8c9f-f9794c37c237")]
 #endif
 
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("FastMember.Signed.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001008bd77a9bd950da020128f20213c4b58ab4dc5dcb690c23737654da3e078c7657bd4d41bbcb14ee24d367cbef6e0addb9d2ed971d539e50857f52a58ced6457212faedb8113df708e3e3025547a7b7f94827395aaefaac835c8fad617e99c21c3622ee98faf9c703c9b12dc7568617f6df66139bfb925fd6bd272defb63de01c0")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -33,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.9")]
-[assembly: AssemblyFileVersion("1.0.0.9")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
